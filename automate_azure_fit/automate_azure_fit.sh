b1b5f82a0a67f40e497f24bc4f44cd8e388e1456
amberlauer <amber.lauer@tunl.duke.edu>
2019-05-30 17:18:10 -0400

    same as previous but fit

--- automate_azure_fit/automate_azure_fit.sh
+++ automate_azure_fit/automate_azure_fit.sh
@@ -9,19 +9,19 @@ cd $cwd
 echo "what to name the output file?"
 read output_name
 output_root=/home/amber/Documents/Tech/azure
-touch  ${output_root}/outputs/${output_name}_fit_outputs.txt
+touch  ${output_name}_fit_outputs.txt
 
-echo    "jpi1 jp2 jpi3 jpi4 res order" >> ${output_root}/outputs/${output_name}_fit_outputs.txt
+echo    "jpi1 jp2 jpi3 jpi4 res order" >> ${output_name}_fit_outputs.txt
 for((a=1; a<=212; a++))
 do
         rm ./current_work.azr
         fit=$(sed -n "$a"'p' ./good_calcs_res4)
         ./automate_azure_fit.o $fit
         mv work_temp.azr current_work.azr
-        echo "$fit" >> ${output_root}/outputs/${output_name}_fit_outputs.txt
-        ./automate_azure_fit.exp | grep -E 'Segment #1|WARNING' >> ${output_root}/outputs/${output_name}_fit_outputs.txt
+        echo "$fit" >> ${output_name}_fit_outputs.txt
+        ./automate_azure_fit.exp | grep -E 'Segment #1|WARNING' >> ${output_name}_fit_outputs.txt
 done
-
+mv ${output_name}_fit_outputs.txt ${output_root}/outputs/
 #WARNING: Denominator less than zero in E=6.95 MeV resonance transformation.  Tranformation may not have been successful.
 
 #xn=$(sed -n ${SLURM_ARRAY_TASK_ID}p ~/sens/reaction_list_305.txt)
